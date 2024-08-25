5a89e04f59550fc713b119b52d2857c287a7cc44
AshutoshGhosh <ghoshashutosh01@gmail.com>
2024-08-25 23:37:35 +0530

    small fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   dbCredentials: {
-    url: process.env.DRIZZLE_DATABASE_URL,
+    url: process.env.DRIZZLE_DATABASE_URL!,
   },
   verbose: true,
   strict: true,
